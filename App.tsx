e19a218e8828db719fd3b4c4fa5c7ee968340005
Artem Denysenko <arten.denisenko@liveperson.com>
2022-11-19 02:13:06 +0200

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from "./components/Dashboard";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Dashboard" component={Dashboard}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Dashboard.tsx
@@ -0,0 +1,35 @@
+import { FAB } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import {StyleSheet, View} from "react-native";
+
+export default function Dashboard () {
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.buttonContainer}>
+                <FAB style={styles.addButton} icon={props => <Icon name="plus" {...props} />} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#5A9BD4',
+    },
+    buttonContainer: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    },
+    addButton: {
+        position: "absolute",
+        bottom: 50,
+        right: 50
+    }
+});
+
